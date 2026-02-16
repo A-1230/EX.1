02447053d72e5ded94f1d8f0c72dd58480e24faf
A-1230 <814725243013@trp.srmtrichy.edu.in>
2026-02-16 14:56:03 +0530

    Implement arithmetic operations and class structure
    Added arithmetic operations and a class definition with a display method.

--- PROGRAM.py
+++ PROGRAM.py
@@ -1,2 +1,20 @@
-x= 10
-print(x)
+a= 5
+b=6
+result= a+b
+print("a+b=",result)
+c=2
+d=result*c
+print("d=",d)
+if d>20:
+    print("d is greater than 20")
+else:
+    print("d is less than 20")
+class myclass:
+    def __init__(self,name,id):
+        self.name=name
+        self.id=id
+    def display(self):
+            print("name:",self.name)
+            print("id:",self.id)
+s1=myclass("reetu",5)
+s1.display()
